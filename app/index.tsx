3964eaaa41e1d22a6089606e4283cb4884377053
yaraaos <yara144ss@gmail.com>
2025-05-29 01:39:40 +0300

    Create Custom Button

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,8 @@
-import { StyleSheet, Text, View } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { StyleSheet, View } from "react-native";
+/*import { SafeAreaView } from "react-native-safe-area-context";*/
+import CustomButton from '@/components/CustomButton';
 
+/*
 export default function Index() {
   return (
     <SafeAreaView style={styles.safeArea}>
@@ -25,4 +27,31 @@ const styles = StyleSheet.create({
     lineHeight: 100,
     color: 'blue',
   }
+})
+*/
+
+export default function HomeScreen() {
+  const handleOrder = () => {
+    alert('Checking out!');
+  };
+
+  return (
+    <View style={styles.container}>
+
+
+      <View style={styles.bottomContainer}>
+        <CustomButton text="Checkout" color="#578600" onPress={handleOrder} />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'flex-end',
+  },
+  bottomContainer : {
+    padding: 24,
+  },
 })
---
+++ components/CustomButton.jsx
@@ -0,0 +1,30 @@
+import { Dimensions, StyleSheet, Text, TouchableOpacity } from 'react-native';
+
+const screenWidth = Dimensions.get('window').width;
+
+export default function CustomButton({ text, onPress, color = '#4CAF50', size = 'medium' }) {
+  const buttonStyles = [
+    styles.button,
+    { backgroundColor: color, paddingVertical: size === 'large' ? 16 : 12 }
+  ];
+
+  return (
+    <TouchableOpacity style={buttonStyles} onPress={onPress}>
+      <Text style={styles.buttonText}>{text}</Text>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  button: {
+    width: screenWidth * 0.9,
+    paddingHorizontal: 24,
+    borderRadius: 8,
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+});
