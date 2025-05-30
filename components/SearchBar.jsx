537222d75af22d2a6405d854a71009bba0c3bf34
yaraaos <yara144ss@gmail.com>
2025-05-30 16:07:04 +0300

    Add Search Icon in SearchBar

--- components/SearchBar.jsx
+++ components/SearchBar.jsx
@@ -1,19 +1,25 @@
+import { Ionicons } from '@expo/vector-icons';
 import React from 'react';
-import { Platform, StyleSheet, TextInput, View, Text } from 'react-native';
+import { Platform, StyleSheet, Text, TextInput, View } from 'react-native';
+
 
 export default function SearchBar({ value, onChangeText, placeholder, helperText }) {
   return (
     <View style={styles.container}>
-      <TextInput
-        style={styles.input}
-        value={value}
-        onChangeText={onChangeText}
-        placeholder={placeholder || "Search..."}
-        placeholderTextColor={Platform.select({
-        ios: '#8E8E93',
-        android: '#888',
-  })}
-      />
+      <View style={styles.inputWrapper}>
+        <Ionicons name="search-outline" size={20} color="#8E8E93" style={styles.icon} />
+        <TextInput
+          style={styles.input}
+          value={value}
+          onChangeText={onChangeText}
+          placeholder={placeholder || "Search..."}
+          placeholderTextColor={Platform.select({
+            ios: '#8E8E93',
+            android: '#888',
+          })}
+        />
+      </View>
+      
       {/* Show helper text when value is empty */}
       {helperText && !value && (
         <Text style={styles.helperText}>{helperText}</Text>
@@ -27,11 +33,23 @@ const styles = StyleSheet.create({
     paddingHorizontal: 16,
     marginTop: 16,
   },
-  input: {
+  inputWrapper: {
+    flexDirection: 'row',
+    alignItems: 'center',
     backgroundColor: '#f8f9fe',
     paddingVertical: 12,
     paddingHorizontal: 16,
     borderRadius: 24,
+  },
+
+  icon: {
+    marginRight: 8,
+  },
+
+  input: {
+    flex: 1,
+    fontSize: 15,
+    color: '#333',
   },
   helperText: {
     marginTop: 4,
