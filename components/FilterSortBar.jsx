e52528d68fb8a1429fc1ea61a2b29c99e7be209f
yaraaos <yara144ss@gmail.com>
2025-06-07 16:19:48 +0300

    Add Icons to Filter/Map/Sort

--- components/FilterSortBar.jsx
+++ components/FilterSortBar.jsx
@@ -1,3 +1,4 @@
+import { Ionicons } from '@expo/vector-icons';
 import React from 'react';
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
@@ -10,15 +11,23 @@ export default function FilterSortBar({
   return (
     <View style={styles.container}>
       <TouchableOpacity style={styles.button} onPress={onSortPress}>
+        <Ionicons name="swap-vertical-outline" size={18} color="#333" style={styles.icon} />
         <Text style={styles.text}>Sort</Text>
       </TouchableOpacity>
 
       <TouchableOpacity style={styles.button} onPress={onFilterPress}>
+         <Ionicons
+          name="options-outline"
+          size={18}
+          color={filtersActive ? '#578600' : '#333'}
+          style={styles.icon}
+        />
         <Text style={styles.text}>Filter</Text>
-        {filtersActive && <View style={styles.dot} />}
+        {filtersActive && <View style={styles.filter} />}
       </TouchableOpacity>
 
-      <TouchableOpacity onPress={onMapPress}>
+      <TouchableOpacity style={styles.button} onPress={onMapPress}>
+        <Ionicons name="map-outline" size={18} color="#333" style={styles.icon} />
         <Text style={styles.text}>Map</Text>
       </TouchableOpacity>
 
@@ -42,12 +51,9 @@ const styles = StyleSheet.create({
     fontWeight: 400,
   },
   
-  // Green dot on filter
-  dot: {               
-    width: 8,
-    height: 8,
+  // Filter turns green when pressed on
+  filter: {   
     borderRadius: 4,
-    backgroundColor: '#578600', 
-    marginLeft: 6,
+    backgroundColor: '#578600',
   },
 });
