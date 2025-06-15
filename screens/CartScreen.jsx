1e693a7f6d73ebc142014a316b635b337d4d8584
yaraaos <yara144ss@gmail.com>
2025-06-15 23:20:40 +0300

    Add Change Quantity in Cart

--- screens/CartScreen.jsx
+++ screens/CartScreen.jsx
@@ -1,11 +1,20 @@
-import React from 'react';
-import { View, Text, FlatList, StyleSheet, Button } from 'react-native';
+import { FlatList, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { useDispatch, useSelector } from 'react-redux';
-import { removeItem } from '../redux/cartSlice';
+import { removeItem, updateQuantity } from '../redux/cartSlice';
 
 export default function CartScreen() {
   const dispatch = useDispatch();
-  const cart = useSelector((state) => state.cart); // type `any` for now
+  const cart = useSelector((state) => state.cart);
+
+  const handleQuantityChange = (item, delta) => {
+    const newQuantity = item.quantity + delta;
+
+    if (newQuantity <= 0) {
+      dispatch(removeItem(item.id));
+    } else {
+      dispatch(updateQuantity({ id: item.id, quantity: newQuantity }));
+    }
+  };
 
   return (
     <View style={styles.container}>
@@ -18,9 +27,20 @@ export default function CartScreen() {
           data={cart}
           keyExtractor={(item) => item.id}
           renderItem={({ item }) => (
-            <View style={styles.item}>
-              <Text>{item.name} x{item.quantity}</Text>
-              <Button title="Remove" onPress={() => dispatch(removeItem(item.id))} />
+             <View style={styles.item}>
+              <Text style={styles.itemText}>{item.name}</Text>
+
+              <View style={styles.controls}>
+                <TouchableOpacity onPress={() => handleQuantityChange(item, -1)} style={styles.btn}>
+                  <Text style={styles.btnText}>−</Text>
+                </TouchableOpacity>
+
+                <Text style={styles.qty}>{item.quantity}</Text>
+
+                <TouchableOpacity onPress={() => handleQuantityChange(item, 1)} style={styles.btn}>
+                  <Text style={styles.btnText}>+</Text>
+                </TouchableOpacity>
+              </View>
             </View>
           )}
         />
@@ -51,5 +71,26 @@ const styles = StyleSheet.create({
     padding: 12,
     backgroundColor: '#f2f2f2',
     borderRadius: 8,
+  },
+  itemText:{
+    fontSize: 16,
+  },
+  controls: { 
+    flexDirection: 'row', 
+    alignItems: 'center' 
+  },
+  btn: {
+    backgroundColor: '#578600',
+    paddingHorizontal: 12,
+    paddingVertical: 4,
+    borderRadius: 4,
+    marginHorizontal: 6,
+  },
+  btnText: { 
+    color: '#fff', 
+    fontSize: 18 
+  },
+  qty: { 
+    fontSize: 16 
   },
 });
